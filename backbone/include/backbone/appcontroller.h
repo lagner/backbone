f931f207b1d6379fd70b3127412421ad238e87c6
Sergey Lagner <spam@lagner.ru>
2019-02-05 01:24:24 +0700

    app controller is useless temporally

--- backbone/include/backbone/appcontroller.h
+++ backbone/include/backbone/appcontroller.h
@@ -2,8 +2,9 @@
 #include <QtCore/QObject>
 #include <backbone/pagepresenter.h>
 #include <backbone/qmlinjector.h>
-#include "global.h"
 #include <QtQml/QJSValue>
+#include "global.h"
+#include "fwd.h"
 
 
 namespace Backbone {
@@ -14,23 +15,9 @@ class AppController
 {
     Q_OBJECT
 public:
-    AppController(
-        QmlInjectorUnq injector,
-        QmlComponentsCache * cache,
-        Router * router,
-        QObject * parent = nullptr);
-
-    Backbone::PagePresenter * resolvePresenter(const QString & name);
-
-
-    Q_INVOKABLE void create(const QUrl & componentUrl, QJSValue callback);
-
-    QObject * create(QQmlComponent * component);
+    AppController(QObject * parent);
 
 private:
-    const QmlInjectorUnq injector_;
-    QmlComponentsCache * cache_ = nullptr;
-    Router * router_ = nullptr;
 };
 
 
